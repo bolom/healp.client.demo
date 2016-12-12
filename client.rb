640471c9e8e7673e80704e66c0f5f07a167f0249
Bolo Michelin <bolo@lordalexworks.com>
2016-12-11 23:21:48 -0400

    Create client.rb

---
+++ client.rb
@@ -0,0 +1,17 @@
+require 'rest-client'
+require 'json'
+
+client_id = 'ce84fa4ea699becba08282996fcde130ad32087827f696711050f6c682ad82bf'
+client_secret = 'db9d1d0d7c25ac79ccfee76a8a6985f0f49516abe522f887e521f3bdfd5dbb45'
+
+response = RestClient.post 'https://healp-backend-staging.herokuapp.com/oauth/token', {
+  grant_type: 'client_credentials',
+  client_id: client_id,
+  client_secret: client_secret
+}
+
+token = JSON.parse(response)["access_token"]
+
+p token
+
+RestClient.get 'https://healp-backend-staging.herokuapp.com/user/search?p=bolo@me.com', { 'Authorization' => "Bearer #{token}" }
