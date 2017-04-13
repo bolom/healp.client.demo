6f9cd0b95c55f5cef9a63b34370645326c2e3fd8
Bolo Michelin <bolo@lordalexworks.com>
2017-04-13 10:04:18 -0400

    Update queue_client.rb

--- queue_client.rb
+++ queue_client.rb
@@ -2,26 +2,25 @@ require 'dotenv'
 Dotenv.load("#{File.dirname(__FILE__)}/.env")
 require_relative "config"
 
-#config = Healp::Config.new(ENV['CLIENT_ID'],ENV['CLIENT_SECRET'],ENV['AUTH_URL'])
+config = Healp::Config.new(ENV['CLIENT_ID'],ENV['CLIENT_SECRET'],ENV['AUTH_URL'])
 
-config = Healp::Config.new(ENV['CLIENT_ID_STAGING'],ENV['CLIENT_SECRET_STAGING'],ENV['AUTH_URL_STAGING'])
+#config = Healp::Config.new(ENV['CLIENT_ID_STAGING'],ENV['CLIENT_SECRET_STAGING'],ENV['AUTH_URL_STAGING'])
 
 client = OAuth2::Client.new(config.client_id, config.client_secret, :site =>  config.oauth_url)
 
-<<<<<<< HEAD
-access_token = client.password.get_token('patient2@lobo.studio', "}S'+<01o")
-=======
-access_token = client.password.get_token('jimmy@lobo.studio', "}S'+<01o")
->>>>>>> origin/master
+access_token = client.password.get_token('bolo+patient@lobo.studio', "azerty")
 
-puts access_token.token
 images = []
-images << File.new("/Users/jimmy/Dungeon.png", 'rb')
-images << File.new("/Users/jimmy/Dungeon.png", 'rb')
+images << File.new("/Users/Michelin/Desktop/test.png", 'rb')
+images << File.new("/Users/Michelin/Desktop/test.png", 'rb')
+
+p access_token.token
 
 response = RestClient.post "#{config.oauth_url}/consultations", {
     access_token: access_token.token,
-    profile_id: 6,
+    profile_id: 115,
+    lat: 44.9817,
+    long: -93.2783,
     assets: images,
     content_type: :json,
 }
